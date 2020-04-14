11f4d19cc7123b04c8d808b6218210cd7b3b4270
YashMunjal <yashmu45@gmail.com>
2020-04-14 23:43:12 +0530

    remove particles

--- js/vendor.js
+++ js/vendor.js
@@ -2,16 +2,11 @@ Reveal.initialize({
     controls: true,
     controlsTutorial: true,
     mouseWheel: true,
+    center:true,
     width: "100%",
     height: "100%",
     margin: 0,
     minScale: 1,
     maxScale: 1,
-    hash:true
 });
 
-
-document.querySelector('#start-journey').addEventListener('click',()=>{
-    Reveal.next();
-    console.log('clicked');
-});
