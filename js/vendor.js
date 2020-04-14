efc03582aaf32e98fe4ae7923e1c2155defa4544
YashMunjal <yashmu45@gmail.com>
2020-04-14 22:10:00 +0530

    new

---
+++ js/vendor.js
@@ -0,0 +1,10 @@
+Reveal.initialize({
+    controls: true,
+    controlsTutorial: true,
+    mouseWheel: true,
+    width: "100%",
+    height: "100%",
+    margin: 0,
+    minScale: 1,
+    maxScale: 1,
+});
