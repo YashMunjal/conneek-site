530ca75baac3365785f6138c9fdead3f304e68f1
YashMunjal <yashmu45@gmail.com>
2020-04-16 23:17:04 +0530

    new

--- js/vendor.js
+++ js/vendor.js
@@ -14,16 +14,35 @@ Reveal.initialize({
     transition: 'zoom',
     controlsBackArrows: 'visible'
 });
+
+//Constants
+
 const idleAudio=document.querySelector('.main-audio');
 const muteBtn=document.querySelector('.slide-one-mute');
+//Other codes
+
+//Ready function
+Reveal.addEventListener( 'ready', function( event ) {
+    muteBtn.children[0].display='none';
+    muteBtn.children[1].display='block';
+} );
+
+
+//Component functions
 muteBtn.addEventListener('click',()=>{
     muteBtn.classList.toggle('active');
+    
     if(muteBtn.classList.contains('active'))
     {
+        muteBtn.children[1].style.display='none';
+        muteBtn.children[0].style.display='block';
+        //console.log(muteBtn.children[1])
         idleAudio.play();
     }
     else if(muteBtn.classList.value==='slide-one-mute')
     {
+        muteBtn.children[0].style.display='none';
+        muteBtn.children[1].style.display='block';
         idleAudio.pause();
     }
     
